2269e79fac07bf20821958696bb4ed4a022ba07f
Jan Carreras Prat <contact@jcarreras.es>
2021-02-10 16:43:07 +0100

    Create Service and its tests using mockery

---
+++ internal/magnet/inspect/service.go
@@ -0,0 +1,30 @@
+package inspect
+
+import (
+	"context"
+	"prevtorrent/internal/magnet"
+)
+
+type Service struct {
+	fileDescriberRepository magnet.FileDescriber
+}
+
+func NewService(fd magnet.FileDescriber) *Service {
+	return &Service{fileDescriberRepository: fd}
+}
+
+type ServiceCMD struct {
+	Magnet string
+}
+
+func (s *Service) Inspect(ctx context.Context, cmd ServiceCMD) error {
+	m, err := magnet.NewMagnet(cmd.Magnet)
+	if err != nil {
+		return err
+	}
+
+	if _, err := s.fileDescriberRepository.DescribeFiles(ctx, m); err != nil {
+		return err
+	}
+	return nil
+}
---
+++ internal/magnet/magnet.go
@@ -0,0 +1,34 @@
+package magnet
+
+import (
+	"context"
+	"errors"
+	"regexp"
+)
+
+var magnetValidationRegexp = regexp.MustCompile("magnet:\\?xt=urn:btih:[a-zA-Z0-9]*")
+
+//go:generate mockery --case=snake --outpkg=clientmocks --output=platform/client/clientmocks --name=FileDescriber
+type FileDescriber interface {
+	DescribeFiles(context.Context, Magnet) ([]File, error)
+}
+
+type Magnet struct {
+	value string
+}
+
+var ErrInvalidMagnetFormat = errors.New("invalid magnet")
+
+func NewMagnet(value string) (Magnet, error) {
+	if !magnetValidationRegexp.Match([]byte(value)) {
+		return Magnet{}, ErrInvalidMagnetFormat
+	}
+	return Magnet{value: value}, nil
+}
+
+func (m Magnet) Value() string {
+	return m.value
+}
+
+type File struct {
+}
