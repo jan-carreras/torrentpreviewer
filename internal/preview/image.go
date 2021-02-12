c8fe2d6b1fb825b04552ebd38e79db8b206dd5bf
Jan Carreras Prat <contact@jcarreras.es>
2021-02-13 00:44:50 +0100

    Fix path where mocks are generated

--- internal/preview/image.go
+++ internal/preview/image.go
@@ -2,13 +2,12 @@ package preview
 
 import "context"
 
-// TODO: Check out how we are supposed to store those mocks. Put them where they make sense
-//go:generate mockery --case=snake --outpkg=inmemorymocks --output=platform/storage/inmemory/inmemorymocks --name=ImageExtractor
+//go:generate mockery --case=snake --outpkg=storagemocks --output=platform/storage/storagemocks --name=ImageExtractor
 type ImageExtractor interface {
 	ExtractImage(ctx context.Context, data []byte, time int) ([]byte, error)
 }
 
-//go:generate mockery --case=snake --outpkg=filemocks --output=platform/storage/file/filemocks --name=ImageRepository
+//go:generate mockery --case=snake --outpkg=storagemocks --output=platform/storage/storagemocks --name=ImageRepository
 type ImageRepository interface {
 	PersistFile(ctx context.Context, id string, data []byte) error
 }
