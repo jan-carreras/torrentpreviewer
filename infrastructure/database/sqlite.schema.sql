ba0657e34b93357ac31ce87c75f0bdd03f050064
Jan Carreras Prat <contact@jcarreras.es>
2021-02-13 23:36:46 +0100

    Create files and media tables

--- infrastructure/database/sqlite.schema.sql
+++ infrastructure/database/sqlite.schema.sql
@@ -5,6 +5,33 @@ CREATE TABLE IF NOT EXISTS torrents
     length      INT         NOT NULL,
     pieceLength INT         NOT NULL,
     raw         BLOB        NOT NULL,
+    created_at  INTEGER     NOT NULL,
 
     PRIMARY KEY (id)
 );
+
+
+CREATE TABLE IF NOT EXISTS files
+(
+    torrent_id varchar(40) NOT NULL,
+    id         int         NOT NULL,
+    name       TEXT        NOT NULL,
+    length     INT         NOT NULL,
+    created_at INTEGER     NOT NULL,
+    PRIMARY KEY (torrent_id, id),
+    FOREIGN KEY (torrent_id) REFERENCES torrents (id)
+);
+
+CREATE TABLE IF NOT EXISTS media
+(
+    id         INTEGER PRIMARY KEY AUTOINCREMENT,
+    torrent_id varchar(40) NOT NULL,
+    file_id    int         NOT NULL,
+    name       TEXT        NOT NULL,
+    length     INT         NOT NULL,
+    source     TEXT,
+    created_at INTEGER     NOT NULL,
+    FOREIGN KEY (torrent_id) REFERENCES torrents (id),
+    FOREIGN KEY (torrent_id, file_id) REFERENCES files (torrent_id, id)
+);
+CREATE INDEX media_torrent_id_file_id ON media (torrent_id, file_id);
