a0d7b3dfc333c4a0c56b7cad2300ec4c6c65a3d2
Jan Carreras Prat <contact@jcarreras.es>
2021-02-22 14:06:18 +0100

    The goroutine leak was solved thus I don't need to monitor it actively
    Something similar might be implemented back again when I add telemetry reporting to the project

--- cmd/cli/torrentprev/bootstrap/bootstrap.go
+++ cmd/cli/torrentprev/bootstrap/bootstrap.go
@@ -6,10 +6,6 @@ import (
 	"prevtorrent/internal/preview/downloadPartials"
 	"prevtorrent/internal/preview/platform/cli"
 	"prevtorrent/internal/preview/unmagnetize"
-	"runtime"
-	"time"
-
-	"github.com/sirupsen/logrus"
 )
 
 func Run() error {
@@ -21,7 +17,6 @@ func Run() error {
 
 	c.config.Print(os.Stdout)
 
-	go goroutineLeak(c)
 	return cli.Run(bus)
 }
 
@@ -54,18 +49,4 @@ func makeCommandBus(c container) *inmemory.SyncCommandBus {
 	)
 
 	return commandBus
-}
-
-func goroutineLeak(c container) {
-	// TODO/BUG: It seems that the torrent library, for some reason, leaks goroutines leading to
-	//           out of memory error. I think it has to do with the logic that checks the hash of
-	//           a chunk, but not sure. It's problematic and I need this here keep an eye on it.
-	//           Sigh. FML.
-	for {
-		goroutines := runtime.NumGoroutine()
-		c.logger.WithFields(logrus.Fields{
-			"goroutines": goroutines,
-		}).Warn("stats")
-		time.Sleep(time.Second)
-	}
 }
