69109947e4ca3b4571b436d0281ed819e0abdd91
Jan Carreras Prat <contact@jcarreras.es>
2021-02-24 01:03:23 +0100

    Removes bootstrap for pubsub command

--- cmd/pubsub/subscriber.go
+++ cmd/pubsub/subscriber.go
@@ -2,12 +2,54 @@ package main
 
 import (
 	"context"
+	"encoding/json"
 	"log"
-	"prevtorrent/cmd/pubsub/bootstrap"
+	"prevtorrent/internal/platform/bus"
+	"prevtorrent/internal/platform/bus/inmemory"
+	"prevtorrent/internal/platform/container"
+	"prevtorrent/internal/preview/downloadPartials"
 )
 
 func main() {
-	if err := bootstrap.Run(context.Background()); err != nil {
+	if err := run(context.Background()); err != nil {
 		log.Fatal(err)
+	}
+}
+
+func run(ctx context.Context) error {
+	c, err := container.NewDefaultContainer()
+	if err != nil {
+		return err
+	}
+
+	commandBus := inmemory.NewSyncCommandBus(c.Logger)
+
+	bus.MakeBindings(commandBus, c) // TODO: Meh.
+
+	downloadPartialsChannel, err := c.Subscriber.Subscribe(ctx, string(downloadPartials.CommandType))
+	if err != nil {
+		return err
+	}
+
+	for {
+		select {
+		case <-ctx.Done():
+			return ctx.Err()
+		case msg, isOpen := <-downloadPartialsChannel:
+			if !isOpen {
+				return nil
+			}
+
+			cmd := downloadPartials.CMD{}
+			err := json.Unmarshal(msg.Payload(), &cmd)
+			if err != nil {
+				return err
+			}
+			err = commandBus.Dispatch(ctx, cmd)
+			if err != nil {
+				return err
+			}
+			msg.ACK()
+		}
 	}
 }
