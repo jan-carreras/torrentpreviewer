a27cb3a174106ac79e6ac47a631afed57696be5d
Jan Carreras Prat <contact@jcarreras.es>
2021-02-24 01:06:46 +0100

    Minor style fixes

--- cmd/pubsub/producer/produce.go
+++ cmd/pubsub/producer/produce.go
@@ -2,37 +2,24 @@ package main
 
 import (
 	"context"
-	"prevtorrent/internal/platform/bus/pubsub"
-	"prevtorrent/internal/preview/downloadPartials"
-	"time"
-
 	"github.com/ThreeDotsLabs/watermill"
 	"github.com/ThreeDotsLabs/watermill-googlecloud/pkg/googlecloud"
-	"github.com/ThreeDotsLabs/watermill/message"
+	"prevtorrent/internal/platform/bus/pubsub"
+	"prevtorrent/internal/preview/downloadPartials"
 )
 
 func main() {
 	logger := watermill.NewStdLogger(false, false)
 
-	publisher, err := googlecloud.NewPublisher(googlecloud.PublisherConfig{
-		ProjectID: "torrentpreview",
-	}, logger)
+	publisher, err := googlecloud.NewPublisher(googlecloud.PublisherConfig{ProjectID: "torrentpreview"}, logger)
 	if err != nil {
 		panic(err)
 	}
-
-	publishMessages(publisher)
-}
-
-func publishMessages(publisher message.Publisher) {
 	commandBus := pubsub.NewPubSubCommandBus(nil, publisher)
-	cmd := downloadPartials.CMD{
-		ID: "c92f656155d0d8e87d21471d7ea43e3ad0d42723",
-	}
-	err := commandBus.Dispatch(context.Background(), cmd)
-	if err != nil {
+	
+	cmd := downloadPartials.CMD{ID: "c92f656155d0d8e87d21471d7ea43e3ad0d42723"}
+
+	if err := commandBus.Dispatch(context.Background(), cmd); err != nil {
 		panic(err)
 	}
-
-	time.Sleep(time.Second)
 }
