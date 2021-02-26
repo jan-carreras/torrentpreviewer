845ba9525cb4b6926d9f5ee40ea6d7c8df8bd157
Jan Carreras Prat <contact@jcarreras.es>
2021-02-26 21:32:47 +0100

    Adds graceful shutdown to Subs command

--- cmd/cli/bus/subs.go
+++ cmd/cli/bus/subs.go
@@ -2,7 +2,10 @@ package main
 
 import (
 	"context"
+	"os"
+	"os/signal"
 	"prevtorrent/internal/platform/container"
+	"syscall"
 )
 
 func main() {
@@ -11,10 +14,19 @@ func main() {
 		panic(err)
 	}
 
-	_ = c.CQRS()
-
 	router := c.CQRSRouter()
-	if err := router.Run(context.Background()); err != nil {
+
+	ctx, cancelCtx := context.WithCancel(context.Background())
+	go gracefulShutdown(cancelCtx)
+
+	if err := router.Run(ctx); err != nil {
 		panic(err)
 	}
+}
+
+func gracefulShutdown(cancelCtx context.CancelFunc) {
+	termChan := make(chan os.Signal)
+	signal.Notify(termChan, syscall.SIGINT, syscall.SIGTERM)
+	<-termChan
+	cancelCtx()
 }
