7561a4ffeb12b1c7ec36ec3e5c5637d431795573
Jan Carreras Prat <contact@jcarreras.es>
2021-03-07 16:38:17 +0100

    Implement graceful shutdown for HTTP server

--- cmd/http/http.go
+++ cmd/http/http.go
@@ -1,11 +1,18 @@
 package main
 
 import (
+	"context"
 	"log"
+	http2 "net/http"
 	"os"
+	"os/signal"
 	"prevtorrent/internal/platform/container"
 	"prevtorrent/internal/platform/services"
 	"prevtorrent/internal/preview/platform/http"
+	"syscall"
+	"time"
+
+	"github.com/sirupsen/logrus"
 )
 
 func main() {
@@ -27,5 +34,28 @@ func run() error {
 		return err
 	}
 
-	return http.Run(s)
+	srv := http.Run(s)
+
+	go func() {
+		if err := srv.ListenAndServe(); err != nil && err != http2.ErrServerClosed {
+			c.Logger().Errorf("listen: %s\n", err)
+		}
+	}()
+
+	waitForGracefulShutdown(c.Logger(), srv)
+
+	return nil
+}
+
+func waitForGracefulShutdown(logger *logrus.Logger, srv *http2.Server) {
+	quit := make(chan os.Signal, 1)
+	signal.Notify(quit, syscall.SIGINT, syscall.SIGTERM)
+	<-quit
+	logger.Info("Shutting down server...")
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+	if err := srv.Shutdown(ctx); err != nil {
+		logger.Errorf("Server forced to shutdown: %v", err)
+	}
 }
